aed53ef80259185d7a98a0bc1ca623449b882ff6
Jaskaran Sohi <jaskaran.sohi@gmail.com>
2021-11-24 14:57:21 -0800

    commit tree a8789cf17c3c488501456e5e94a5f8af215ae4eb

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+const findMatching = (driverArray, driver) => {
+  // iterate over our array
+  // return an array that matches the string driver
+  return driverArray.filter((human) => {
+    // check where human === driver
+    // case insensitive ?  ==> make all one case
+    return human.toLowerCase() === driver.toLowerCase();
+  });
+};
+
+const fuzzyMatch = (driverArray, firstLetters) => {
+  return driverArray.filter((driver) => {
+    // test if the current driver starts with the firstLetters argument?
+    const testLength = firstLetters.length;
+    return driver.startsWith(firstLetters);
+    // //return (
+    //   driver.slice(0, testLength).toLowerCase() === firstLetters.toLowerCase()
+    // );
+  });
+};
+
+const matchName = (driverArray, driver) => {
+  return driverArray.filter((driverObj) => {
+    // test if the key of 'name' is equal to our driver arg
+
+    // how to get the value of key name out of driverObj??
+    return driverObj.name === driver;
+  });
+};
